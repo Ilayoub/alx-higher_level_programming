d3be472e1a3e93e8c011380ecbb093b67df85d4a
Ilayoub <jrindou.ayoub@gmail.com>
2023-09-05 15:46:21 -0700

    just try me

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
 
-"""Prints all possible different combinations of two digits. The two digits must
-    be different01 and 10 are considered identical"""
+"""Prints all possible different combinations of two digits.
+The two digits must be different01 and 10 are considered identical"""
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
         if digit1 == 8 and digit2 == 9:
