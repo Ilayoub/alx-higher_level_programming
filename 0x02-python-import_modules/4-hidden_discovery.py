11a5780e1482855a8f9f3431c3a9741b825abc4a
Ilayoub <jrindou.ayoub@gmail.com>
2023-09-07 09:51:26 -0700

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """The program that prints all the names defined by the compiled module hidden_4.pyc"""
+    """The program prints all the names defined by the compiled hidden_4.pyc"""
     import hidden_4
 
     names = dir(hidden_4)
