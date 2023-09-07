41dd2bf900913c662222dc15d6cc4e4b0861356f
Ilayoub <jrindou.ayoub@gmail.com>
2023-09-07 09:56:44 -0700

    102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 
 def magic_calculation(a, b):
-    """The program matches bytecode def magic_calculation(a, b)"""
+    """The program matches bytecode def magic_calculation"""
     from magic_calculation_102 import add, sub
 
     if a < b:
