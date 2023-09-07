9061aa9f342cd2ded2b5247454d826d3f949d8d7
Ilayoub <jrindou.ayoub@gmail.com>
2023-09-07 15:33:21 -0700

    101

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-
-    """program that prints #pythoniscool in the standard output"""
 __import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+"""program that prints #pythoniscool in the standard output"""
