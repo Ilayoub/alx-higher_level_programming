2ba39d57e6ecc6daf314768b0cfdaa3199dce650
Ilayoub <jrindou.ayoub@gmail.com>
2023-09-07 09:53:26 -0700

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     """The program imports all functions from the file
-    calculator_1.py and handles basic operations"""
+ calculator_1.py and handles basic operations"""
     from calculator_1 import add, sub, mul, div 
     import sys
 
