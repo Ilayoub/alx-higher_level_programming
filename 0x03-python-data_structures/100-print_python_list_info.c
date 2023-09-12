d113f0959301b918ddd0b3b43f8f4bb52ecddd6d
Ilayoub <jrindou.ayoub@gmail.com>
2023-09-11 17:17:05 -0700

    Python Data Structures

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - the function prints some basic info about Python lists
+ * @p: The PyObject list
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, k;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (k = 0; k < size; k++)
+	{
+		printf("Element %d: ", k);
+
+		obj = PyList_GetItem(p, k);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
+}
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,7 @@
 
 
 def new_in_list(my_list, idx, element):
-    """The program replaces an element in a list at
-    a specific position without modifying
-    the original list"""
+    """The program replaces an element in a list at a specific position"""
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
