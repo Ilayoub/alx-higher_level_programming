5744a20afdb77a1f14a5d5ef701084673af913a8
Ilayoub <jrindou.ayoub@gmail.com>
2023-09-11 17:23:32 -0700

    Python Data Structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 
 def new_in_list(my_list, idx, element):
-    """The program replaces an element in a list at a specific position"""
+    """The program replaces an element in a copied list at a specific position"""
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
