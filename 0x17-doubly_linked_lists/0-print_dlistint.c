a6d10f5b1360b7cb3eaf4e762e389936c2d9fa13
Ilayoub <jrindou.ayoub@gmail.com>
2023-09-15 15:48:38 -0700

    c - doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,7 +9,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int c;
 
-	ct = 0;
+	c = 0;
 
 	if (h == NULL)
 		return (c);
@@ -25,4 +25,4 @@ size_t print_dlistint(const dlistint_t *h)
 	}
 
 	return (c);
-}
+}	
