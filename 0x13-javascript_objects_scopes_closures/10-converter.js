ec5904d69ca288ca4ffbec8457d8ae44466d7c05
Ilayoub <jrindou.ayoub@gmail.com>
2024-06-19 14:15:28 -0700

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,4 +2,8 @@
 // The function converts a number from base 10 
 // to another base passed as argument
 
-exports.converter = function (base) { return num => num.toString(base); };
+exports.converter = function (base) {
+  return function (n) {
+    return (n.toString(base));
+  };
+};
--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,20 +2,8 @@
 // The function classes Square that defines a square
 // and inherits from Rectangle of 4-rectangle.js
 
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
   }
 };
