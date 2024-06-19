2321b7998c5a41ff4608d0b848749b4b8d3d5191
Ilayoub <jrindou.ayoub@gmail.com>
2024-06-19 14:20:26 -0700

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// The function converts a number from base 10 
+// The function converts a number from base 10
 // to another base passed as argument
 
 exports.converter = function (base) {
