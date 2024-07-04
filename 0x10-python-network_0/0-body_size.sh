5356a2ac564fbf158be7803b591a1f40d47145e6
Ilayoub <jrindou.ayoub@gmail.com>
2024-07-04 14:54:07 -0700

    Python - Network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request and
 # displays the size of the body of the response
-curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
+
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
