9d42009b99d042755a493f423044315375b5d7d1
Ilayoub <jrindou.ayoub@gmail.com>
2024-08-07 12:50:46 -0700

    JavaScript - Web jQuery

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+// JavaScript script that fetches the character name from URL
+
+let url = 'https://swapi.co/api/people/5/?format=json';
+$.get(url, function (data, stat) {
+  $('div#character').text(data.name);
+});
